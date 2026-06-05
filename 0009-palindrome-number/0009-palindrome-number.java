d562138c4852f35195ad228cfa97eebefe9c22b5
B-Rohit-1 <115752035+B-Rohit-1@users.noreply.github.com>
2026-06-05 22:12:05 +0530

    Time: 5 ms (78.65%), Space: 45.9 MB (71.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int t = x,s=0;
+        while(t>0){
+            s=(s*10)+(t%10);
+            t=t/10;
+        }
+    if(s == x){
+        return true;
+    }else{
+        return false;
+    }    
+    }
+}
